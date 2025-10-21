528c10ed5fd70d80998d3cb42f2163c842b9b4cf
Minalina Pukhart <mishapukhart@gmail.com>
2025-10-21 19:41:33 +0300

    second

---
+++ ishop1/App.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Shop from './components/Shop';
+
+ReactDOM.render(
+  <Shop name="Ozon" address="Leninskaya street" />, 
+  document.getElementById('container') 
+);
---
+++ ishop1/components/Shop.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+
+import './Shop.css';
+
+class Shop extends React.Component {
+
+  render() {
+    return (
+      <div className='wrapper'>
+        <h1 className='shop_name'>{this.props.name}</h1>
+        <p className='shop_address'>{this.props.address}</p>
+      </div>
+    );
+  }
+
+}
+
+export default Shop;
