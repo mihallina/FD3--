bc44a69d77c733fdd2c02d9c9771338075cfc8de
Minalina Pukhart <mishapukhart@gmail.com>
2025-11-04 13:40:04 +0300

    ishop2

--- ishop1/components/Product.js
+++ ishop1/components/Product.js
@@ -4,7 +4,10 @@ import "./Product.css";
 class Product extends React.Component {
   render() {
     return (
-      <div className="card">
+      <div
+        className={`card ${this.props.selected ? "selected" : ""}`}
+        onClick={() => this.props.onSelect(this.props.id)}
+      >
         <div className="img-wrapper">
           <img src={this.props.url} alt="" />
           <div className="text-wrapper">
@@ -12,7 +15,22 @@ class Product extends React.Component {
             <p className="price">{this.props.price}</p>
           </div>
         </div>
-        <p className="instock">{this.props.instock} items left</p>
+        <div className="right">
+          <p className="instock">{this.props.instock} items left</p>
+          <button
+            className="delete-btn"
+            onClick={(e) => {
+              e.stopPropagation();
+              if (
+                window.confirm("Are you sure you want to delete this product?")
+              ) {
+                this.props.onDelete(this.props.id);
+              }
+            }}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     );
   }
--- ishop1/components/Shop.js
+++ ishop1/components/Shop.js
@@ -4,14 +4,46 @@ import "./Shop.css";
 import Product from "./Product";
 
 class Shop extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      products: this.props.products,
+      selectedIds: new Set()
+    }
+    this.deleteProduct = this.deleteProduct.bind(this);
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  deleteProduct(key) {
+    this.setState({
+      products: this.state.products.filter((el) => el.key !== key)
+    })
+  }
+
+  changeColor(key) {
+    this.setState(prevState => {
+      const newSelected = new Set(prevState.selectedIds);
+      if(newSelected.has(key)){
+        newSelected.delete(key);
+      } else {
+        newSelected.add(key);
+      }
+      return {selectedIds: newSelected}
+    }) 
+  }
+
   render() {
-    const productCode = this.props.products.map((pr) => (
+    const productCode = this.state.products.map((pr) => (
       <Product
         key={pr.key}
+        id={pr.key}
         url={pr.url}
         name={pr.name}
         price={pr.price}
         instock={pr.instock}
+        selected={this.state.selectedIds.has(pr.key)}
+        onSelect={this.changeColor}
+        onDelete={this.deleteProduct}
       />
     ));
 
