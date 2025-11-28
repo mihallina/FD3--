fb78018ad66cd645d9d95ad5eb22aa09291f076d
Minalina Pukhart <mishapukhart@gmail.com>
2025-11-28 11:38:33 +0300

    mobile

---
+++ Mobile/components/MobileClient.js
@@ -0,0 +1,130 @@
+﻿import React from "react";
+import "./MobileClient.css";
+import events from "./Events";
+
+class MobileClient extends React.PureComponent {
+  famRef = React.createRef();
+  nameRef = React.createRef();
+  otchRef = React.createRef();
+  balanceRef = React.createRef();
+
+  state = {
+    isEditing: this.props.client.isEditing || false,
+  };
+
+  toggleEdit = () => {
+    const willEditing = !this.state.isEditing;
+
+    this.setState({ isEditing: willEditing }, () => {
+      if (willEditing && this.famRef.current) {
+        this.famRef.current.focus();
+      }
+    });
+  };
+
+  save = () => {
+    const { client } = this.props;
+
+    const fam = this.famRef.current ? this.famRef.current.value : client.fam;
+    const name = this.nameRef.current
+      ? this.nameRef.current.value
+      : client.name;
+    const otch = this.otchRef.current
+      ? this.otchRef.current.value
+      : client.otch;
+    const balance = this.balanceRef.current
+      ? parseFloat(this.balanceRef.current.value)
+      : 0;
+
+    const updatedClient = {
+      ...client,
+      fam,
+      name,
+      otch,
+      balance,
+      isEditing: false,
+    };
+
+    events.emit("client:edit", updatedClient);
+    this.setState({ isEditing: false });
+  };
+
+  cancel = () => {
+    const { client } = this.props;
+    if (client.fam === "[новый]") {
+      events.emit("client:delete", client.id);
+    } else {
+      this.setState({ isEditing: false });
+    }
+  };
+
+  delete = () => {
+    if (window.confirm(`Удалить клиента "${this.props.client.fam}"?`)) {
+      events.emit("client:delete", this.props.client.id);
+    }
+  };
+
+  render() {
+    let {client} = this.props;
+    console.log("MobileClient id=" + client.id + " render");
+
+    return (
+      <tr className="row">
+        <td>
+          {this.state.isEditing ? (
+            <input type="text" defaultValue={client.fam} ref={this.famRef} />
+          ) : (
+            client.fam
+          )}
+        </td>
+        <td>
+          {this.state.isEditing ? (
+            <input type="text" defaultValue={client.name} ref={this.nameRef} />
+          ) : (
+            client.name
+          )}
+        </td>
+        <td>
+          {this.state.isEditing ? (
+            <input type="text" defaultValue={client.otch} ref={this.otchRef} />
+          ) : (
+            client.otch
+          )}
+        </td>
+        <td>
+          {this.state.isEditing ? (
+            <input
+              type="text"
+              defaultValue={client.balance}
+              ref={this.balanceRef}
+            />
+          ) : (
+            client.balance
+          )}
+        </td>
+        <td className={client.balance <= 0 ? "blocked" : "active"}>
+          {client.balance <= 0 ? "blocked" : "active"}
+        </td>
+        <td>
+          {this.state.isEditing ? (
+            <div className="save-cancel">
+              <input type="button" value="✓" className="save" onClick={this.save} />
+              <input type="button" value="×" className="cancel" onClick={this.cancel} />
+            </div>
+          ) : (
+            <input
+              type="button"
+              value="редактировать"
+              onClick={this.toggleEdit}
+            />
+          )}
+        </td>
+        <td>
+          <input type="button" value="удалить" onClick={this.delete} />
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default MobileClient;
---
+++ Mobile/components/MobileCompany.js
@@ -0,0 +1,121 @@
+﻿import React from "react";
+import PropTypes from "prop-types";
+
+import MobileClient from "./MobileClient";
+
+import "./MobileCompany.css";
+import events from "./Events";
+
+class MobileCompany extends React.PureComponent {
+  state = {
+    clients: this.props.clients.map((client) => ({ ...client })),
+    filter: "all",
+  };
+
+  componentDidMount() {
+    events.on("client:delete", this.handleDelete);
+    events.on("client:edit", this.handleEdit);
+    events.on("client:add", this.handleAdd);
+  }
+
+  componentWillUnmount() {
+    events.removeListener("client:delete", this.handleDelete);
+    events.removeListener("client:edit", this.handleEdit);
+    events.removeListener("client:add", this.handleAdd);
+  }
+
+  handleDelete = (id) => {
+    this.setState((prev) => ({
+      clients: prev.clients.filter((c) => c.id !== id),
+    }));
+  };
+
+  handleEdit = (client) => {
+    this.setState((prev) => ({
+      clients: prev.clients.map((c) =>
+        c.id === client.id ? { ...client } : c
+      ),
+    }));
+  };
+
+  handleAdd = () => {
+    const newId =
+      this.state.clients.reduce((max, c) => Math.max(max, c.id), 0) + 1;
+    const newClient = {
+      id: newId,
+      fam: "[новый]",
+      name: "",
+      otch: "",
+      balance: 0,
+      isEditing: true,
+    };
+
+    this.setState((prev) => ({ clients: [...prev.clients, newClient] }));
+  };
+
+  setFilter = (filter) => {
+    this.setState({ filter });
+  };
+
+  getFilteredClients = () => {
+    const { clients, filter } = this.state;
+    if (filter == "active") {
+      return clients.filter((c) => c.balance > 0);
+    }
+    if (filter == "blocked") {
+      return clients.filter((c) => c.balance <= 0);
+    } else {
+      return clients;
+    }
+  };
+
+  render() {
+    console.log("MobileCompany render");
+
+    const clientsToShow = this.getFilteredClients();
+    const clientsCode = clientsToShow.map((client) => (
+      <MobileClient key={client.id} client={client} />
+    ));
+
+    return (
+      <div className="MobileCompany">
+        <input
+          type="button"
+          value="Все"
+          onClick={() => this.setFilter("all")}
+        />
+        <input
+          type="button"
+          value="Активные"
+          onClick={() => this.setFilter("active")}
+        />
+        <input
+          type="button"
+          value="Заблокированные"
+          onClick={() => this.setFilter("blocked")}
+        />
+        <table>
+          <thead>
+            <tr>
+              <th>Фамилия</th>
+              <th>Имя</th>
+              <th>Отечество</th>
+              <th>Баланс</th>
+              <th>Статус</th>
+              <th>Редактировать</th>
+              <th>Удалить</th>
+            </tr>
+          </thead>
+          <tbody>{clientsCode}</tbody>
+        </table>
+        <input
+          type="button"
+          value="Добавить клиента"
+          onClick={this.handleAdd}
+        />
+      </div>
+    );
+  }
+}
+
+export default MobileCompany;
